a8891e67136a6436ceb1c9ee9b26039becd0ab3f
sakkarakaya <sakkarakaya@icloud.com>
2021-05-05 17:11:03 +0200

    added useRef

--- src/components/Searchbox/index.js
+++ src/components/Searchbox/index.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import './style.css'
 
 const Searchbox = ({setSearchText}) => {
-    const [inputText, setInputText] = useState("")
-    const getInput = (v) => {
-        setInputText(v) 
-     }
+    // const [inputText, setInputText] = useState("")
+    const inputRef = useRef()
+    
+    // const getInput = (v) => {
+    //     setInputText(v) 
+    //  }
     
     return (
 
         <div className="searchbox">
-            <input onChange={e => getInput(e.target.value)}/>
-            <button onClick={() => setSearchText(inputText)}>Search</button>
+            {/* <input onChange={e => getInput(e.target.value)}/> */}
+            <input ref={inputRef}/>
+            <button onClick={() => setSearchText(inputRef?.current?.value)}>Search</button>
         </div>
     )
 }
